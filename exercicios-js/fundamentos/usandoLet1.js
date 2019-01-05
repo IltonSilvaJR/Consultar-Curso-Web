42f72ed6869de30a953dc77cf83c4f758b75c2de
IltonSivaJR <iltonbatista2018@gmail.com>
2019-01-05 14:46:12 -0200

    Declaração de variavel com Let

---
+++ exercicios-js/fundamentos/usandoLet1.js
@@ -0,0 +1,16 @@
+var numero = 1
+{
+    //Let ta dentro apenas no bloco
+    let numero = 2
+    
+    console.log('Dentro 1= ', numero);
+}
+console.log('Fora 2=', numero)
+
+var numero = 6
+{
+    //Let ta dentro apenas no bloco
+    let numero2 = 8
+    console.log('Dentro 3= ', numero);
+}
+console.log('Fora 4=', numero)
