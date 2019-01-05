03a349e1cbb2619fc708c38cb9f2d6c19a9a79f1
IltonSivaJR <iltonbatista2018@gmail.com>
2019-01-05 15:21:21 -0200

    Entedendo o Hoisting

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,40 @@
+console.log("a1 = ", a)
+
+var a = 2
+
+console.log("a2 = ", a)
+
+
+/*
+ERRO SE FOR LET
+*/
+console.log('b = ',b)
+
+let b = 2
+
+console.log("b = ", b)
+/**
+ * em muitas linguagem isso daria problema
+ * mas em java script da certo sem gerar problema
+ * mas fica undefined ou seja
+ * não definida mas isso iria gerar um erro
+ * 
+ * 
+ *
+    console.log("a1 = ", a)
+    //var a = 2
+    console.log("a2 = ", a)
+ */
+
+ // ERRO NO CONSOLE FINAL
+ /*
+ function teste(){
+console.log("b1 = ", b)
+
+var b = 20
+
+console.log("b2 = ", b)
+ }
+teste()
+console.log('b =', b)
+*/
